29ceaab1c235157e167fb396a4c913a7de1a880e
Anssi Lepikko <anssi.lepikko@gmail.com>
2021-03-23 23:59:19 +0200

    2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,8 +3,9 @@ import Person from './components/Person'
 
 const App = (props) => {
   // All persons in the phonebook
-  const [ persons, setPersons] = useState(props.persons) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState(props.persons)
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -18,19 +19,29 @@ const App = (props) => {
     const personObject = {
       id: persons.length + 1,
       name: newName,
-      number: 1234567890
+      number: newNumber
     }
-  // Add new person to persons-state
-  setPersons(persons.concat(personObject))
-  // Cleans the input field
-  setNewName('')
+    // Add new person to persons-state
+    setPersons(persons.concat(personObject))
+    console.log('New person added to phonebook:', personObject)
+    
+    // Clean the input fields
+    setNewName('')
+    setNewNumber('')
   }
 
-  // What happens when there is a change in the input field
+  // What happens when there is a change in the name
   const handleNameChange = (event) => {
     console.log(event.target.value)
     // Prints input event value to the input field
     setNewName(event.target.value)
+  }
+
+  // What happens when there is a change in the number field
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    // Prints input event value to the input field
+    setNewNumber(event.target.value)
   }
 
   const findPerson = (person) => {
@@ -39,10 +50,10 @@ const App = (props) => {
     // Return true if found, false if not found
     const found = persons.find(item => item.name === person)
     if (found) {
-      console.log('FindPerson found', found)
+      console.log('FindPerson found:', found)
       return true
     }
-    console.log('FindPerson', person, 'not found')
+    console.log('FindPerson', person, 'not in phonebook')
     return false
   }
 
@@ -52,6 +63,9 @@ const App = (props) => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
