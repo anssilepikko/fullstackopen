94132bd7d6e9cd2fd60ccb2a18a6ae78e4edebf7
Anssi Lepikko <anssi.lepikko@gmail.com>
2021-04-20 23:31:55 +0300

    2.16: puhelinluettelo step8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,108 +2,113 @@ import React, { useEffect, useState } from 'react'
 import Numbers from './components/Numbers.js'
 import Form from './components/Form.js'
 import Filter from './components/Filter.js'
-import noteService from './services/notes.js'
+import noteService from './services/persons.js'
 
 
 const App = (props) => {
-  // All persons in the phonebook
+  // Tilojen määritys
+  // Kaikki henkilöt puhelinluettelossa
   const [persons, setPersons] = useState(props.persons)
-  // State-variables for field entries
+  // Tilamuuttujat inputeille
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
 
+  // Haetaan henkilöt serveriltä ja asetetaan
+  // ne tilamuuttujaan
   useEffect(() => {
     noteService
       .getAll()
-      .then(response => {
-        console.log('Data retrieved from: /persons')
-        setPersons(response.data)
+      .then(serverPersons => {
+        setPersons(serverPersons)
       })
   }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
 
-    // Do nothing if name not given
+    // Palataan, jos nimeä ei ole annettu
     if (newName === '') return
 
-    // Check if person (name) already exists
+    // Tarkistetaan löytyykö nimi jo luettelosta
     if (findPerson(newName)) {
-      // Alert window template string
+      // Ilmoitusikkuna, jos löytyy
       window.alert(`${newName} is already added to phonebook`)
       return
     }
 
-    // Object for person
+    // Henkilö-objekti, jossa nimi ja numero
     const personObject = {
       name: newName,
       number: newNumber
     }
 
+    // Lähetetään henkilön tiedot oliona serverille
+    // Vastaanotetaan serveriltä saatu vastaus,
+    // jossa tiedot ja lisätään ne ohjelman tilaan
     noteService
       .create(personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        console.log('New person added to phonebook:', personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
       })
+    console.log('New person added to phonebook:', personObject)
 
-  // Reset the states (input fields)
-  setNewName('')
-  setNewNumber('')
-  console.log('Fields reset')
-}
+    // Input-kenttien nollaus
+    setNewName('')
+    setNewNumber('')
+    console.log('Fields reset')
+  }
 
-// What happens when there is a change in the name
-const handleNameChange = (event) => {
-  console.log(event.target.value)
-  // Prints input event value to the input field
-  setNewName(event.target.value)
-}
+  // Mitä tapahtuu, kun nimikentässä havaitaan tapahtuma
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    // Printataan tapahtuman arvo eli kirjain kenttään
+    setNewName(event.target.value)
+  }
 
-// What happens when there is a change in the number field
-const handleNumberChange = (event) => {
-  console.log(event.target.value)
-  // Prints input event value to the input field
-  setNewNumber(event.target.value)
-}
+  // Mitä tapahtuu, kun numerokentässä havaitaan tapahtuma
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    // Printataan tapahtuman arvo eli numero kenttään
+    setNewNumber(event.target.value)
+  }
 
-// What happens when there is a change in the filter field
-const handleFilterChange = (event) => {
-  //console.log(event.target.value)
-  // Prints input event value to the input field
-  setNewFilter(event.target.value)
-}
+  // Mitä tapahtuu, kun filtterikentässä havaitaan tapahtuma
+  const handleFilterChange = (event) => {
+    //console.log(event.target.value)
+    // Printataan tapahtuman arvo eli kirjain kenttään
+    setNewFilter(event.target.value)
+  }
 
-const findPerson = (person) => {
-  console.log(persons)
-  console.log('FindPerson input:', person)
-  // Loop trough objects and find an object with the same name
-  // Return true if found, false if not found
-  const found = persons.find(item => item.name === person)
-  console.log('Found:', found)
-  return found
+  const findPerson = (person) => {
+    console.log(persons)
+    console.log('FindPerson input:', person)
+    // Käydään nimet läpi
+    // Palautetaan true, jos löytyy
+    // Palautetaan false, jos ei löydy
+    const found = persons.find(item => item.name === person)
+    console.log('Found:', found)
+    return found
+  }
 
-}
+  const filterNumbers = () => {
+    if (newFilter === '') return persons
+    // Etsitään hakuehtoa vastaavat nimet ja palautetaan ne
+    const filtered = persons.filter(person => person.name.toUpperCase().includes(newFilter.toUpperCase()))
+    return filtered
+  }
 
-const filterNumbers = () => {
-  if (newFilter === '') return persons
-  // Serach matching strings from names
-  const filtered = persons.filter(person => person.name.toUpperCase().includes(newFilter.toUpperCase()))
-  return filtered
-}
-
-return (
-  <div>
-    <h2>Phonebook</h2>
-    <Form addPerson={addPerson} name={newName} handleName={handleNameChange} number={newNumber} handleNumber={handleNumberChange} />
-    <h2>Numbers</h2>
-    <Filter name={newFilter} handleName={handleFilterChange} />
-    <ul>
-      <Numbers numbers={filterNumbers()} />
-    </ul>
-  </div>
-)
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Form addPerson={addPerson} name={newName} handleName={handleNameChange} number={newNumber} handleNumber={handleNumberChange} />
+      <h2>Numbers</h2>
+      <Filter name={newFilter} handleName={handleFilterChange} />
+      <ul>
+        <Numbers numbers={filterNumbers()} />
+      </ul>
+    </div>
+  )
 
 }
 
---
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+// Hakee tiedot serveriltä ja palauttaa reponsen
+// data-kentän, jossa tiedot ovat taulukkomuodossa
+const getAll = () => {
+    // Axios palauttaa promisen
+    const request =  axios.get(baseUrl)
+    console.log('persons.js / getAll / request', request)
+    // Koska then:in parametri palauttaa suoraan arvon
+    // response.data, on funktion getAll palauttama
+    // promise sellainen, että jos HTTP-kutsu onnistuu,
+    // antaa promise takaisinkutsulleen HTTP-pyynnön
+    // mukana olleen datan
+    return request.then(response => response.data)
+
+}
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put('${baseUrl}/${id}', newObject)
+    return request.then(response => response.data)
+}
+
+// Koska olion kenttien nimet ovat samat kuin
+// niiden arvon määrittelevien muuttujien nimet,
+// voidaan olion määrittely kirjoittaa tiivimmässä muodossa:
+export default { getAll, create, update }
